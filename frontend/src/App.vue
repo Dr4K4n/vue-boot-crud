22cc2bc6222e6af4a8fad1698574e0688708c1f2
Stefan Erichsen <stefan@balticfinance.com>
2018-05-14 11:03:20 +0200

    anonymized application

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,9 @@
 <template>
     <div id="main-content">
-    <img src="./assets/wiebke_marvin.jpg" height="400">
-    <hello-world newlyweds="Wiebke &amp; Marvin" weddingdate="01.06.2018">
+        <hello-world newlyweds="Hans &amp; Wurst" weddingdate="01.08.2018">
       <h4>Wir brauchen eure Hilfe!</h4>
-      <p>Auf der Hochzeitsfeier im <a href="http://www.ristorante-giuliano.de/" target="_blank">Ristorante
-        Giuliano</a> möchten wir Wiebke &amp; Marvin dabei helfen die Gäste untereinander bekannt zu machen. Sie
+            <p>Auf der Hochzeitsfeier im Ristorante möchten wir Hans &amp; Wurst dabei helfen die Gäste untereinander
+                bekannt zu machen. Sie
         sollen anhand von kurzen Beschreibungen oder Fragen erraten welcher Gast oder auch welche Gäste gemeint
         sind und diese kurz vorstellen.</p>
       <p>Hierfür sind wir noch auf der Suche nach weiteren Beschreibungen und Fragen, z.B.</p>
@@ -12,18 +11,6 @@
     </hello-world>
     <h4>Vielen Dank!</h4>
     <h4>Die Hochzeitsplaner</h4>
-    <p>Silke &amp; Detlef, Renate &amp; Peter, Deike &amp; Stefan, Svenja, Cevin und Babette</p>
-    <!--
-    <hr>
-    <h2>Hochzeits Quartett</h2>
-    <p>
-      Ich möchte meiner Schwester und ihrem baldigen Ehemann ein individuell gestaltetes
-      <a href="https://www.meinspiel.de/quartett-mit-fotos-gestalten-drucken?tid=1000" target="_blank">Quartett</a> zur
-      Hochzeit schenken!
-    </p>
-    <p>Bei den Spielkarten handelt es sich um EUCH, die Gäste!</p>
-    <quartett-karte></quartett-karte>
-    -->
   </div>
 </template>
 
